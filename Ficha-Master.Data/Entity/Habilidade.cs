69f0eba0aae6fb151a1de23d747051701407ff1a
Gabrielnolasco <gabrielncostasouza@outlook.com>
2024-11-19 13:31:30 -0300

    Criando um Enum para os tipos de Habilidade

--- Ficha-Master.Data/Entity/Habilidade.cs
+++ Ficha-Master.Data/Entity/Habilidade.cs
@@ -1,10 +1,12 @@
+using Ficha_Master.Data.Enum;
+
 namespace Ficha_Master.Data.Entity
 {
     public class Habilidade
     {
         public Guid Id { get; set; }
         public string Nome { get; set;}
-        public string Categoria {get; set;} /*Define se a habilidade é de Classe / Raça / Origem / Poderes - Combate - Destino - Magia - Concedidos - Tormenta  */
+        public TipoHabilidades TipoHabilidade {get; set;} /* Define se a habilidade é de Classe / Raça / Origem / Poderes - Combate - Destino - Magia - Concedidos - Tormenta  */
         public string Descricao { get; set;}
     }
 }
---
+++ Ficha-Master.Data/Enum/TipoHabilidades.cs
@@ -0,0 +1,14 @@
+namespace Ficha_Master.Data.Enum
+{
+    public enum TipoHabilidades
+    {
+        Habilidade_de_Classe = 1,
+        Habilidade_de_Raça = 2,
+        Habilidade_de_Origem = 3,
+        Poderes_de_Combate = 4,
+        Poderes_de_Destino = 5,
+        Poderes_de_Magia = 6,
+        Poderes_Concedidos = 7,
+        Poderes_da_Tormenta = 8
+    }
+}
