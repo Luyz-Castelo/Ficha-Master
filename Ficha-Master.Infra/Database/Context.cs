1bdbf4914106584c2d0c931df06143a650833eac
Gabrielnolasco <gabrielncostasouza@outlook.com>
2024-11-21 13:46:02 -0300

    Adicionando contexto para Mesas e  Fichas

--- Ficha-Master.Infra/Database/Context.cs
+++ Ficha-Master.Infra/Database/Context.cs
@@ -8,6 +8,11 @@ namespace Ficha_Master.Infra.Database
         public DbSet<Usuario> Usuario { get; set; }
         public DbSet<Personagem> Personagem { get; set; }
         public DbSet<Ficha> Ficha { get; set; }
+        public DbSet<Habilidade> Habilidade {get; set;}
+        public DbSet<Magias> Magias { get; set; }
+        public DbSet<Itens> Itens { get; set; }
+        public DbSet<Mesa> Mesa { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -15,6 +20,12 @@ namespace Ficha_Master.Infra.Database
 
             modelBuilder.Entity<Usuario>().HasMany(u => u.Fichas).WithOne(f => f.Usuario).HasForeignKey(f => f.UsuarioId).HasPrincipalKey(u => u.Id);
             modelBuilder.Entity<Personagem>().HasOne(p => p.Ficha).WithOne(f => f.Personagem);
+            /* Não sei se está correto fiz com ajuda do ChatGPT */
+            modelBuilder.Entity<Ficha>().HasMany(f => f.MochilaItens).WithOne().HasForeignKey("FichaId");
+            modelBuilder.Entity<Ficha>().HasMany(f => f.Magias).WithOne().HasForeignKey("FichaId");
+            modelBuilder.Entity<Ficha>().HasMany(f => f.HabilidadesPoderes).WithOne().HasForeignKey("FichaId");
+            modelBuilder.Entity<Mesa>().HasMany(m => m.Jogador).WithOne().HasForeignKey("MesaId");
+            modelBuilder.Entity<Mesa>().HasMany(m => m.Jogador).WithOne().HasForeignKey("MesaId");
         }
     }
 }
